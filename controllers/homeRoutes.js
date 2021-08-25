5758b200280b7f530712d7358154804ca326ca90
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-25 00:33:17 -0500

    restructured files and added app.use public thing to server to get css to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project, User, Post } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      posts, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -40,7 +40,7 @@ router.get('/post/:id', async (req, res) => {
     const post = postData.get({ plain: true });
 
     res.render('post', {
-      ...project,
+      ...post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
