8607c98cf05c750afd41e234500bbe96e1d76e04
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-25 22:31:08 -0500

    updatated styling working on signup routes handlebars and js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -78,4 +78,12 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+  if (req.session.logged_In) {
+    res.redirect('/signup');
+    return;
+  }
+  res.render('signup');
+});
+
 module.exports = router;
